70905caf1ce9790017a87bb594a7932d94cbc8d5
shehryar <shehryarkashan@gmail.com>
2023-07-13 23:38:31 +0500

    it

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import HomePage from '../components/home/Home'
-import About from '@/components/about/About'
-import Blog from '@/components/blog/Blog'
-import Services from '@/components/services/Services'
-import Contact from '@/components/contact/Contact'
-import Clients from '@/components/clients/Clients'
-import Footer from '@/components/footer/footer'
-import Team from '@/components/team/Team'
-import HomeSlider from "@/components/slider/Slider"
-import FooterTop from '@/components/footerTop/FooterTop'
-import Profile from '@/components/profile/Profile'
+import About from '../components/about/About'
+import Blog from '../components/blog/Blog'
+import Services from '../components/services/Services'
+import Contact from '../components/contact/Contact'
+import Clients from '../components/clients/Clients'
+import Footer from '../components/footer/footer'
+import Team from '../components/team/Team'
+import HomeSlider from "../components/slider/Slider"
+import FooterTop from '../components/footerTop/FooterTop'
+import Profile from '../components/profile/Profile'
 
 export default function Home() {
   return (
